cbf5195b5a6dae9a34c3bfdf7dee42b2bece036b
congminhle44 <46514752+congminhle44@users.noreply.github.com>
2021-09-27 15:34:23 +0700

    recommit

--- public/js/index.js
+++ public/js/index.js
@@ -1,93 +1,96 @@
+/** @format */
+
 const socket = io();
 const { name, room } = $.deparam(window.location.search);
-socket.on("connect", () => {
-  socket.emit("INFO_FROM_CLIENT_TO_SERVER", {
+socket.on('connect', () => {
+  socket.emit('INFO_FROM_CLIENT_TO_SERVER', {
     name,
     room,
   });
-  socket.emit("notiConnect", {
-    from: "Admin",
-    createdAt: moment(new Date()).format("LT"),
+  socket.emit('notiConnect', {
+    from: 'Admin',
+    createdAt: moment(new Date()).format('LT'),
     text: `${name} joined the room chat`,
   });
 });
-socket.on("toOtherUser", (message) => {
+socket.on('toOtherUser', (message) => {
   const text = message.text;
   const liTag = `<li>${text}</li>`;
-  $("#message").append(liTag);
+  $('#message').append(liTag);
 });
 
-socket.on("toAllUser", (msg) => {
-  const template = $("#message-template").html();
+socket.on('toAllUser', (msg) => {
+  const template = $('#message-template').html();
+  console.log(msg);
   const html = Mustache.render(template, {
     from: msg.from,
-    createdAt: moment(msg.createdAt).format("LT"),
+    createdAt: moment(msg.createdAt).format('LT'),
     text: msg.text,
   });
-  $("#message").append(html);
+  $('#message').append(html);
 });
 
-socket.on("USER_LIST", (msg) => {
+socket.on('USER_LIST', (msg) => {
   const users = msg.users;
-  const ol = $("<ol></ol>");
+  const ol = $('<ol></ol>');
 
   users.forEach((user) => {
     const li = $(`<li>${user.name}</li>`);
     ol.append(li);
   });
-  $("#users").html(ol);
+  $('#users').html(ol);
 });
 
-socket.on("LOCATION_FROM_CLIENT_TO_SERVER", (msg) => {
-  const template = $("#location-message-template").html();
+socket.on('LOCATION_FROM_CLIENT_TO_SERVER', (msg) => {
+  const template = $('#location-message-template').html();
   const html = Mustache.render(template, {
     from: msg.from,
-    createdAt: moment(msg.createdAt).format("LT"),
+    createdAt: moment(msg.createdAt).format('LT'),
     url: `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.870735636578!2d${msg.lng}!3d${msg.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTDCsDQ5JzE2LjMiTiAxMDbCsDQyJzQyLjEiRQ!5e0!3m2!1svi!2s!4v1595640229835!5m2!1svi!2s`,
   });
-  $("#message").append(html);
+  $('#message').append(html);
 });
 
-socket.on("USER_DISCONNECT", (msg) => {
-  const template = $("#message-template").html();
+socket.on('USER_DISCONNECT', (msg) => {
+  const template = $('#message-template').html();
   const html = Mustache.render(template, {
     from: msg.from,
-    createdAt: moment(msg.createdAt).format("LT"),
+    createdAt: moment(msg.createdAt).format('LT'),
     text: msg.text,
   });
   const users = msg.users;
-  const ol = $("<ol></ol>");
+  const ol = $('<ol></ol>');
 
   users.forEach((user) => {
     const li = $(`<li>${user.name}</li>`);
     ol.append(li);
   });
-  $("#users").html(ol);
-  $("#message").append(html);
+  $('#users').html(ol);
+  $('#message').append(html);
 });
 
-$("#message-form").on("submit", (e) => {
+$('#message-form').on('submit', (e) => {
   e.preventDefault();
-  const text = $("[name=message]").val();
+  const text = $('[name=message]').val();
 
-  $("[name=message]").val("");
+  $('[name=message]').val('');
 
-  if (text !== "") {
-    socket.emit("fromClient", {
+  if (text !== '') {
+    socket.emit('fromClient', {
       from: name,
       text,
       createdAt: new Date().getTime(),
     });
   }
-  $("#message").scrollTop($("#message").height());
+  $('#message').scrollTop($('#message').height());
 });
 
-$("#send-location").on("click", (e) => {
-  if (!navigator.geolocation) return alert("Your browser is too old");
+$('#send-location').on('click', (e) => {
+  if (!navigator.geolocation) return alert('Your browser is too old');
   navigator.geolocation.getCurrentPosition((position) => {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    socket.emit("LOCATION_FROM_CLIENT_TO_SERVER", {
+    socket.emit('LOCATION_FROM_CLIENT_TO_SERVER', {
       from: name,
       lat,
       lng,
